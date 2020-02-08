4f9b93098fe278fa3dfb9b18e8e21c035db5be4e
Necritto <onetopyr@mail.ru>
2020-02-08 23:45:55 +0300

    Feature: styles added [ref:no-ref]

---
+++ src/assets/js/common.js
@@ -0,0 +1,5 @@
+// Portfolio link
+document.querySelector('.submenu').parentElement.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.submenu').classList.toggle('toggle');
+});
